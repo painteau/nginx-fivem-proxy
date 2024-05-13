77741c59dc1ca8de7632ee3e3258a744a2ae2fa1
KaO <52933590+painteau@users.noreply.github.com>
2024-05-13 20:34:13 +0200

    good

---
+++ docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -eu
+sed -i "s/FIVEM_SERVER_IP/${FIVEM_SERVER_IP}/g" /etc/nginx/nginx.conf
+sed -i "s/FIVEM_SERVER_PORT/${FIVEM_SERVER_PORT}/g" /etc/nginx/nginx.conf
+sed -i "s/FIVEM_PROXY_DOMAIN/${FIVEM_PROXY_DOMAIN}/g" /etc/nginx/nginx.conf
+sed -i "s/FIVEM_PROXY_PORT/${FIVEM_PROXY_PORT}/g" /etc/nginx/nginx.conf
+exec "$@"
